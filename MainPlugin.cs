7ea9c253e3f8e6ba47445b51eb94dfd636990ccb
Enrique Ramirez <enriquein@gmail.com>
2022-03-05 14:34:34 -0400

    Rename project to distinguish it from the original.

--- MainPlugin.cs
+++ MainPlugin.cs
@@ -8,13 +8,13 @@ namespace AddAmountOfRecipePer1min
     [BepInProcess("DSPGAME.exe")]
     public class MainPlugin : BaseUnityPlugin
     {
-        public const string ModGuid = "com.wokdok.plugins.addamountofrecipeper1min";
+        public const string ModGuid = "com.enriquein.plugins.addamountofrecipeper1min_continued";
         public const string ModName = "AddAmountOfRecipePer1min";
         public const string ModVersion = "1.0.0";
 
         public void Awake()
         {
-            var harmony = new Harmony("com.wokdok.plugins.addamountofrecipeper1min.patch");
+            var harmony = new Harmony("com.enriquein.plugins.addamountofrecipeper1min_continued.patch");
             harmony.PatchAll(typeof(Patch_UIItemTip_SetTip));
             harmony.PatchAll(typeof(Patch_UIItemTip_OnDisable));
         }
